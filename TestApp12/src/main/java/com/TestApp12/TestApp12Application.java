a3e3d27e5bdecfcece247ab456af55691d707ecf
unknown <maheshchowdary0523@gmail.com>
2023-07-27 18:54:02 +0530

    hello

---
+++ TestApp12/src/main/java/com/TestApp12/TestApp12Application.java
@@ -0,0 +1,13 @@
+package com.TestApp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApp12Application.class, args);
+	}
+
+}
---
+++ TestApp12/src/test/java/com/TestApp12/TestApp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestApp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
